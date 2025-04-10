ed008894157b8f9e47dd49507d02cc4cdf62a659
Sander Greni <sander@datek.no>
2025-04-10 07:57:03 +0200

    and even more tests

--- json_stuff/src/lib.rs
+++ json_stuff/src/lib.rs
@@ -57,3 +57,89 @@ pub mod json_stuff
         Ok(json.dump())
     }
 }
+
+// ************************************************************************************************
+// ********************************************* TESTS ********************************************
+// ************************************************************************************************
+
+#[cfg(test)]
+mod tests
+{
+    use std::{collections::HashMap, os::fd::RawFd};
+
+    use crate::json_stuff::{get_login_response, get_request_code, get_username, parse_json};
+
+    #[test]
+    fn test_parse_valid_json()
+    {
+        let valid_json_str = r#"{"valid":"json"}"#;
+        let valid_json = parse_json(valid_json_str);
+        assert!(valid_json.is_some());
+        
+        assert_eq!(valid_json.unwrap().to_string(), valid_json_str);
+    }
+    
+    #[test]
+    fn test_parse_invalid_json()
+    {
+        let valid_json_str = r#""invalid":"json""#;
+        let valid_json = parse_json(valid_json_str);
+        assert!(valid_json.is_none());
+    }
+    
+    #[test]
+    fn test_get_username_from_login()
+    {
+        let valid_json_str = r#"{"request":"login","user":{"username":"sander"}}"#;
+        let valid_json = parse_json(valid_json_str);
+        assert!(valid_json.is_some());
+
+        match get_username(&valid_json.unwrap(), "user")
+        {
+            Ok(username) => assert_eq!(username, "sander"),
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_get_username_from_message()
+    {
+        let valid_json_str = r#"{"request":"message","recipient":{"username":"sander"},"payload":"Hei Sander"}"#;
+        let valid_json = parse_json(valid_json_str);
+        assert!(valid_json.is_some());
+
+        match get_username(&valid_json.unwrap(), "recipient")
+        {
+            Ok(username) => assert_eq!(username, "sander"),
+            Err(_) => assert!(false),
+        }
+    }
+    
+    #[test]
+    fn test_get_request_code()
+    {
+        let valid_json_str = r#"{"request":"message","recipient":{"username":"sander"},"payload":"Hei Sander"}"#;
+        let valid_json = parse_json(valid_json_str);
+        assert!(valid_json.is_some());
+
+        match get_request_code(&valid_json.unwrap())
+        {
+            Ok(req_code) => assert_eq!(req_code, "message"),
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_get_login_response()
+    {
+        let mut users: HashMap<String, RawFd> = HashMap::new();
+        users.insert("sander".to_owned(), 1);
+        users.insert("per".to_owned(), 2);
+
+        match get_login_response(&users)
+        {
+            Ok(valid_json) => assert_eq!(r#"{"response":"login","users":["sander","per"]}"#, valid_json.as_str()),
+            Err(_) => assert!(false)
+        }
+    }
+}
